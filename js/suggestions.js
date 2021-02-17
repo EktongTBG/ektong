87fe65af2e43cc1f4a95371c996ffcd8cfdc3147
EktongTBG <79195020+EktongTBG@users.noreply.github.com>
2021-02-17 17:31:17 +0800

    .
    .

---
+++ js/suggestions.js
@@ -0,0 +1,19 @@
+let suggestions = [
+    
+    "Mark Alexis Sarmiento",
+    "Designer",
+    "Developer",
+    "Web Designer",
+    "Web Developer",
+    "Login Form in HTML & CSS",
+    "Hotel Transylvania (2012)",
+    "How to learn HTML & CSS",
+    "How to learn JavaScript",
+    "How to became Freelancer",
+    "How to became Web Designer",
+    "How to start Gaming Channel",
+    "How to start YouTube Channel",
+    "What does HTML stands for?",
+    "What does CSS stands for?",
+    "No results found",
+];
